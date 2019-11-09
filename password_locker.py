ad1ea4a92dc7fe8c2ec3715f9796662f016ebc2f
Dancanoduor <dancanoduor1990@gmail>
2019-11-09 15:04:39 +0300

    Add Function to save a new user account

--- password_locker.py
+++ password_locker.py
@@ -4,3 +4,7 @@ from user_credentials import User, Credential
 def create_user(fname,lname,password):
 	new_user = User(fname,lname,password)
 	return new_user
+
+def save_user(user):
+	User.save_user(user)
+
