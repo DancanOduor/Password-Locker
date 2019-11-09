35cd2e2754ac5ad000bc3ecfb59b20d92c57c7fb
Dancanoduor <dancanoduor1990@gmail>
2019-11-09 16:26:43 +0300

    Add Test to if check the initialization/creation of user instances is properly done

--- user_credentials_test.py
+++ user_credentials_test.py
@@ -6,3 +6,8 @@ class TestUser(unittest.TestCase):
 
 	def setUp(self):
 		self.new_user = User('Dancan','Od\'uo\'r','28750d')
+
+def test__init__(self):
+		self.assertEqual(self.new_user.first_name,'Dancan')
+		self.assertEqual(self.new_user.last_name,'Od\'uo\'r')
+		self.assertEqual(self.new_user.password,'28750d')
