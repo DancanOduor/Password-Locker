9259653cccc181276c0fafe910c2cde62d7668d1
Dancanoduor <dancanoduor1990@gmail>
2019-11-09 15:11:59 +0300

    Add Function to generate a password automatically

--- password_locker.py
+++ password_locker.py
@@ -12,3 +12,7 @@ def save_user(user):
 def verify_user(first_name,password):
 	checking_user = Credential.check_user(first_name,password)
 	return checking_user
+
+def generate_password(self):
+	gen_pass = Credential.generate_password(self)
+	return gen_pass
