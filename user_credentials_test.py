6869fc6722be2a344755d6cee8737eaf81f128e0
Dancanoduor <dancanoduor1990@gmail>
2019-11-09 16:30:17 +0300

    Add Test to check if the new users info is saved into the users list

--- user_credentials_test.py
+++ user_credentials_test.py
@@ -11,3 +11,7 @@ def test__init__(self):
 		self.assertEqual(self.new_user.first_name,'Dancan')
 		self.assertEqual(self.new_user.last_name,'Od\'uo\'r')
 		self.assertEqual(self.new_user.password,'28750d')
+
+def test_save_user(self):
+		self.new_user.save_user()
+		self.assertEqual(len(User.users_list),1)
